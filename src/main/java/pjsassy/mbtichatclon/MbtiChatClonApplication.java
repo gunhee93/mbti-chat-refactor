b4a1aacdad7bed28f1afc0057b6155e901ab3810
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-05-03 22:32:17 +0900

    Initial commit

---
+++ src/main/java/pjsassy/mbtichatclon/MbtiChatClonApplication.java
@@ -0,0 +1,13 @@
+package pjsassy.mbtichatclon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiChatClonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiChatClonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pjsassy/mbtichatclon/MbtiChatClonApplicationTests.java
@@ -0,0 +1,13 @@
+package pjsassy.mbtichatclon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiChatClonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
