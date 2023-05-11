34c70ed286e658abc3089da468b0715f043981ff
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-05-11 22:48:58 +0900

    dd

--- src/main/java/pjsassy/mbtichatclon/chatting/repository/ChattingRepository.java
+++ src/main/java/pjsassy/mbtichatclon/chatting/repository/ChattingRepository.java
@@ -3,5 +3,5 @@ package pjsassy.mbtichatclon.chatting.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pjsassy.mbtichatclon.chatting.domain.message.Message;
 
-public interface ChattingRepository extends JpaRepository<Message, Long> {
+public interface ChattingRepository extends JpaRepository<ChattingRoom, Long> {
 }
